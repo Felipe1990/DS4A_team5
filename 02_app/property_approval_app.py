c6141af9d900e4e9e43a55d972dde3f11583c8ea
Felipe <felipe.avella.n@hotmail.com>
2020-05-06 16:34:09 -0500

    Update app

--- 02_app/property_approval_app.py
+++ 02_app/property_approval_app.py
@@ -3,6 +3,7 @@ import numpy as np
 import streamlit as st
 import altair as alt
 import time
+import utils
 
 # sidebar
 
@@ -18,7 +19,9 @@ st.sidebar.markdown("""
                     ## Characteristics of the property:
                     """)
 
-property_address = st.sidebar.text_input('Property Adress')
+property_address = st.sidebar.text_input('Property Adress', value='SCN Quadra 02')
+property_city = st.sidebar.text_input('Property City', value='Brasilia')
+
 
 run_button = st.sidebar.button('Get result')
 result = st.sidebar.empty()
@@ -26,39 +29,54 @@ result = st.sidebar.empty()
 
 if run_button:
     result.empty()
-    progress_bar = st.sidebar.progress(0)
+    # progress_bar = st.sidebar.progress(0)
     status_text = st.sidebar.empty()
 
-    for i in range(1, 101):
-        status_text.text("%i%% Complete" % i)
-        progress_bar.progress(i)
-        time.sleep(0.02)
+    # for i in range(1, 101):
+    #     status_text.text("%i%% Complete" % i)
+    #     progress_bar.progress(i)
+    #     time.sleep(0.02)
 
-    status_text.empty()
-    progress_bar.empty()
-    
-    if np.random.normal()>0.5:
-        result.markdown('Accepted')
-    else: 
-        result.markdown('Rejected')
+    # status_text.empty()
+    # progress_bar.empty()
 
 else:
     st.empty()
 
-# main pane
+# main panels
 
 st.write("""
          # Property approval model
         """)
 
-df = pd.DataFrame(
-    np.random.randn(loan_size, 3),
-    columns=['a', 'b', 'c'])
+# df = pd.DataFrame(
+#     np.random.randn(loan_size, 3),
+#     columns=['a', 'b', 'c'])
 
-c = alt.Chart(df).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+# c = alt.Chart(df).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 
 if run_button:
-    st.altair_chart(c, use_container_width=True)
+    
+    with st.spinner('Wait for it...'):
+        property_location = utils.geo_code(property_address, property_city)
+        df = pd.DataFrame({'lat': [np.round(property_location['lat'], 5)],  
+                        'lon': [np.round(property_location['lng'], 5)]},
+                            index=[0])
+        cod_sector = utils.convert_geo_to_sector_code(property_location, {'Distrito Federal': 'df'}, '../data/sharp')
+    
+    st.map(df, zoom=14)        
+    st.write(property_location)
+    # st.text(property_location['state_name'])
+    st.text(cod_sector)
+
+    if np.random.normal()>0.5:
+        st.success('Accepted')
+    else: 
+        st.info('Rejected')
+    
+    
+    # st.altair_chart(c, use_container_width=True)
 else:
     st.empty()
+
 
---
+++ 02_app/utils.py
@@ -0,0 +1,103 @@
+import os
+import requests
+from shapely.geometry import Point
+import geopandas as gpd
+
+def geo_code(address, city):
+    """
+    Geo code address sing open maps API
+    
+    Parameters
+    ------------
+    
+    address: str
+        Address as clear as possible, better to check first if it can be found in open street search engine
+    city: str
+        Name of the city
+        
+    Returns
+    ---------
+    
+    results: dict
+        dictionary with latitude, longitud and state name information
+    
+    """
+    parameters = {'key': os.environ.get("CON_KEY"),
+                  'location': '{0:s}, {1:s}, Brazil'.format(address, city),
+                  'thumbMaps': False,
+                  'maxResults': 1
+                 }
+    
+    response = requests.get('http://www.mapquestapi.com/geocoding/v1/address', params=parameters)
+
+    assert response.status_code==200, 'Review address or internet connection'
+    
+    results = response.json()['results'][0]['locations'][0]['latLng']
+    results['state_name'] = response.json()['results'][0]['locations'][0]['adminArea3']
+
+    assert results['lat']!=39.78373, 'Review address or internet connection'
+
+    return results 
+
+
+def convert_geo_to_sector_code(geo_code_output, states_dict, path_to_shapes):
+    """
+    Conver latitud, longitud and state reference to sector code
+
+
+    Parameters
+    ------------
+
+    geo_code_output: dict
+        output of geo_code function
+    states_dict: dict
+        correspondence of states names
+    path_to_shapes: str
+        path to folder containing shapes
+
+    Returns
+    ---------
+
+    sector code: str
+    """
+
+    coordinate_point = Point(geo_code_output['lng'], geo_code_output['lat'])
+    
+    state_in_response = geo_code_output['state_name']
+    state_name = states_dict[state_in_response]
+    
+    assert state_name in os.listdir(path_to_shapes), 'There is no shape available to reference this address'
+    
+    file_name = [file for file in os.listdir(path_to_shapes+'/'+state_name) if file.find('.shp')>0][0]
+    census_sector = gpd.read_file(path_to_shapes+'/{0:s}/{1:s}'.format(state_name, file_name), encoding='latin1')
+
+    sector_code = census_sector.loc[census_sector.contains(coordinate_point), 'CD_GEOCODI'].values[0]
+
+    return sector_code
+
+# {'sp',
+#  'es',
+#  'rj',
+#  'mg',
+#  'pr',
+#  'rs',
+#  'ba',
+#  'sc',
+#  'al',
+#  'go',
+#  'pe',
+#  'pi',
+#  'am',
+#  'df',
+#  'mt',
+#  'ms',
+#  'ce',
+#  'ma',
+#  'se',
+#  'rn',
+#  'pb',
+#  'to',
+#  'pa',
+#  'ro',
+#  'rr',
+#  'ac'}
