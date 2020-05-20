ddd95bc475def62fc275c024efa7cc9f8bfb5c74
Felipe Tellez <f.tellez130@uniandes.edu.co>
2020-05-20 11:45:31 -0500

    ajustes scrip .py modelos agregados en tests.ipynb

--- 03_scripts/01_data_processing/data_preparation_classes.py
+++ 03_scripts/01_data_processing/data_preparation_classes.py
@@ -7,6 +7,7 @@ import pandas as pd
 from shapely.geometry import Point
 import os
 
+
 class CurrentLabels:
     """
     Start with the labeled databse, including lat and long and add the code sector (census_code) column
@@ -20,48 +21,54 @@ class CurrentLabels:
                    .fillna(value={'model_decision': 'NA_string',
                                   'analyst_decision': 'NA_string'})
                    .dropna(subset=['coordinates']).reset_index(drop=True)
-        )
+                   )
 
     def create_long_lant_cols(self):
-        self.df['long'] = pd.to_numeric(self.df.coordinates.str.split(',', expand=True).loc[:,0].str.replace('\(', ''))
-        self.df['lat'] = pd.to_numeric(self.df.coordinates.str.split(',', expand=True).loc[:,1].str.replace('\)', ''))
+        self.df['long'] = pd.to_numeric(self.df.coordinates.str.split(
+            ',', expand=True).loc[:, 0].str.replace('\(', ''))
+        self.df['lat'] = pd.to_numeric(self.df.coordinates.str.split(
+            ',', expand=True).loc[:, 1].str.replace('\)', ''))
         self.df['state'] = self.df.concat.apply(lambda row: row.split(',')[-1].lower().strip())
 
         self.df['coordinate_point'] = pd.Series([], dtype='object')
         for idx, row in self.df.iterrows():
             self.df.loc[idx, 'coordinate_point'] = Point(row.long, row.lat)
-        
+
     def drop_cols(self):
         self.df = self.df.drop(columns=['zip_code', 'coordinates', 'Unnamed: 0'])
 
     def join_sector_code(self):
-                
+
         def join_code_sector_inner(df):
-            
+
             assert len(df.state.unique()) == 1, ('Más de un estado presente en la base')
             state = df.state.unique()[0]
-            
+
             inner_df = df.copy()
-            
+
             if state in os.listdir('data/sharp'):
-                file_name = [file for file in os.listdir('data/sharp/'+state) if file.find('.shp')>0][0]
-                census_sector = gpd.read_file('data/sharp/{0:s}/{1:s}'.format(state, file_name), encoding='latin1')
-                inner_df['census_code'] = inner_df['coordinate_point'].apply(lambda row: census_sector.loc[census_sector.contains(row), 'CD_GEOCODI'].values).str[0]
-            
-            else :
+                file_name = [file for file in os.listdir(
+                    'data/sharp/'+state) if file.find('.shp') > 0][0]
+                census_sector = gpd.read_file(
+                    'data/sharp/{0:s}/{1:s}'.format(state, file_name), encoding='latin1')
+                inner_df['census_code'] = inner_df['coordinate_point'].apply(
+                    lambda row: census_sector.loc[census_sector.contains(row), 'CD_GEOCODI'].values).str[0]
+
+            else:
                 inner_df['census_code'] = np.nan
-                
+
             return inner_df
-            
+
         self.df = (self.df
-                    .assign(state_index=lambda x: x.state)
-                    .groupby('state_index')
-                    .apply(lambda df: join_code_sector_inner(df))
-                    .reset_index(drop=True)
+                   .assign(state_index=lambda x: x.state)
+                   .groupby('state_index')
+                   .apply(lambda df: join_code_sector_inner(df))
+                   .reset_index(drop=True)
                    )
-    
+
     def save_df(self, path_to_save='data/procesada/data_with_index.pkl'):
-        self.df.to_pickle(path_to_save)
+        self.df.to_pickle(path_to_save, protocol= 4)
+
 
 class DataWithDups:
 
@@ -76,32 +83,36 @@ class DataWithDups:
               .format(self.df
                       .duplicated(subset=['lat', 'long', 'concat'], keep=False)
                       .mean())
-                      )
+              )
+
     def unify_decision(self):
         self.df = (self.df
                    .assign(final_decision=lambda x: np.where(x.analyst_decision.isin(['A', 'R']),
-                                                             x.analyst_decision, 
+                                                             x.analyst_decision,
                                                              np.where(x.model_decision.isin(['A', 'R']),
                                                                       x.model_decision,
                                                                       'undefined')))
                    .drop(columns=['model_decision', 'analyst_decision'])
-                  )
-    
+                   )
+
     def remove_duplicates(self):
         self.df = (self.df
                    .assign(uno=1)
-                   .groupby(['state','census_code', 'concat', 'lat', 'long','final_decision'])
+                   .groupby(['state', 'census_code', 'concat', 'lat', 'long', 'final_decision'])
                    .agg(count=('uno', sum))
                    .reset_index()
                    .assign(random_index=lambda x: np.random.normal(size=x.shape[0]))
-                   .sort_values(by=['state', 'concat', 'lat', 'long','count', 'random_index'], ascending=False)
+                   .sort_values(by=['state', 'concat', 'lat', 'long', 'count', 'random_index'], ascending=False)
                    .drop_duplicates(subset=['census_code', 'concat', 'state', 'lat', 'long'], keep='first')
                    .drop(columns=['count', 'random_index'])
                    .reset_index(drop=True)
                    )
 
     def save_df(self, path_to_save='data/procesada/data_with_index_nodups.pkl'):
-        self.df.to_pickle(path_to_save)
+        self.df.to_pickle(path_to_save, protocol= 4)
+
+    def remove_undefined(self):
+        self.df = self.df[self.df.final_decision != 'undefined']
 
 
 class FinalLabelsWithSector:
@@ -109,19 +120,21 @@ class FinalLabelsWithSector:
     def __init__(self, path_to_file='data/procesada/data_with_index_nodups.pkl'):
         self.df = pd.read_pickle(path_to_file)
         self.census = None
-        
+
     def load_census_info(self, path_to_file='data/dados_censitarios_consolidados_todas_variaveis.csv'):
         self.census = pd.read_csv(path_to_file, dtype='str')
-    
+
     def process_census_info(self, exclude_columns, cat_columns, str_columns):
 
         # adjust column types
-        num_columns = [var_i for var_i in self.census.columns if var_i not in cat_columns + str_columns]
+        num_columns = [
+            var_i for var_i in self.census.columns if var_i not in cat_columns + str_columns]
         for cat_i in cat_columns:
             self.census[cat_i] = self.census[cat_i].astype('category')
 
         for num_i in num_columns:
-            self.census[num_i] = pd.to_numeric(self.census[num_i].str.replace(',', '.'), errors='coerce')   
+            self.census[num_i] = pd.to_numeric(
+                self.census[num_i].str.replace(',', '.'), errors='coerce')
 
         # drop excluded columns
         self.census = self.census.drop(columns=exclude_columns)
@@ -130,11 +143,12 @@ class FinalLabelsWithSector:
         self.census = pd.get_dummies(self.census, columns=cat_columns)
 
     def join_census_info(self):
-        self.df = self.df.merge(self.census,left_on='census_code', right_on='Cod_setor', how='left')
+        self.df = self.df.merge(self.census, left_on='census_code',
+                                right_on='Cod_setor', how='left')
         self.census = None
 
     def save_df(self, path_to_save='data/procesada/data_plus_census.pkl'):
-        self.df.to_pickle(path_to_save)
+        self.df.to_pickle(path_to_save, protocol= 4)
 
 
 class DataLabelAndFeatures:
--- 03_scripts/01_data_processing/initial_data_process.py
+++ 03_scripts/01_data_processing/initial_data_process.py
@@ -10,50 +10,53 @@ keys_aws = {'AWS_ACCESS_KEY': ' AKIARQBCIP76L6XVXIVQ',
             'S3_BUCKET_NAME': 'ds4ateam5'}
 
 #
-# labels = CurrentLabels('data/Copy of registry.csv')
-# labels.adjust_nas()
-# labels.create_long_lant_cols()
-# labels.drop_cols()
-# labels.join_sector_code()
-# labels.save_df()
-# labels = None
-# print('initial labes processed')
-
+labels = CurrentLabels('data/Copy of registry.csv')
+labels.adjust_nas()
+labels.create_long_lant_cols()
+labels.drop_cols()
+labels.join_sector_code()
+labels.save_df()
+labels = None
+print('initial labes processed')
 #
-# data_dups = DataWithDups()
-# data_dups.drop_nas_in_sector()
-# data_dups.print_dups()
-# data_dups.unify_decision()
-# data_dups.remove_duplicates()
-# data_dups.save_df()
-# data_dups = None
-# print('data duplication process finished')
 
-#
-# exclude_columns = ['Unnamed: 0', 'Nome_Grande_Regiao', 'Nome_da_RM',
-#                    'Nome_da_UF ', 'Nome_da_meso', 'Nome_da_micro',
-#                    'Nome_do_bairro', 'Nome_do_distrito', 'Nome_do_municipio',
-#                    'Nome_do_subdistrito']
-                   
-# cat_columns = ['Tipo_setor', 'Situacao_setor',
-#                'Cod_Grandes Regiões','Cod_RM']
-# str_columns = ['Cod_UF', 'Cod_bairro', 'Cod_distrito', 
-#                'Cod_meso','Cod_micro', 'Cod_municipio',
-#                'Cod_subdistrito', 'Cod_setor']
+data_dups = DataWithDups()
+data_dups.drop_nas_in_sector()
+data_dups.print_dups()
+data_dups.unify_decision()
+data_dups.remove_duplicates()
+data_dups.remove_undefined()
+data_dups.save_df()
+data_dups = None
+print('data duplication process finished')
 
-# data_with_label = FinalLabelsWithSector()
-# data_with_label.load_census_info()
-# data_with_label.process_census_info(exclude_columns=exclude_columns, cat_columns=cat_columns, str_columns=str_columns)
-# data_with_label.join_census_info()
-# data_with_label.save_df()
-# data_with_label = None
-# print('add census labels')
+
+exclude_columns = ['Unnamed: 0', 'Nome_Grande_Regiao', 'Nome_da_RM',
+                   'Nome_da_UF ', 'Nome_da_meso', 'Nome_da_micro',
+                   'Nome_do_bairro', 'Nome_do_distrito', 'Nome_do_municipio',
+                   'Nome_do_subdistrito']
+
+cat_columns = ['Tipo_setor', 'Situacao_setor',
+               'Cod_Grandes Regiões', 'Cod_RM']
+str_columns = ['Cod_UF', 'Cod_bairro', 'Cod_distrito',
+               'Cod_meso', 'Cod_micro', 'Cod_municipio',
+               'Cod_subdistrito', 'Cod_setor']
+
+data_with_label = FinalLabelsWithSector()
+
+data_with_label.load_census_info()
+data_with_label.process_census_info(
+    exclude_columns=exclude_columns, cat_columns=cat_columns, str_columns=str_columns)
+data_with_label.join_census_info()
+data_with_label.save_df()
+data_with_label = None
+print('add census labels')
 # #
 
 list_vars = ['DOMICILIO_RENDA_V001', 'DOMICILIO_RENDA_V005', 'DOMICILIO_RENDA_V006',
-                  'DOMICILIO_RENDA_V007', 'DOMICILIO_RENDA_V008', 'DOMICILIO_RENDA_V009',
-                  'DOMICILIO_RENDA_V010',  'DOMICILIO_RENDA_V011', 'DOMICILIO_RENDA_V012',
-                  'DOMICILIO_RENDA_V013', 'DOMICILIO_RENDA_V014']
+             'DOMICILIO_RENDA_V007', 'DOMICILIO_RENDA_V008', 'DOMICILIO_RENDA_V009',
+             'DOMICILIO_RENDA_V010',  'DOMICILIO_RENDA_V011', 'DOMICILIO_RENDA_V012',
+             'DOMICILIO_RENDA_V013', 'DOMICILIO_RENDA_V014']
 name_total_var = 'total_domicilios'
 
 data_features = DataLabelAndFeatures()
